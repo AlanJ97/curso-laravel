0480f91c2ae51575d0c7e0333ccd395fd9035832
Alan <alannavaseg_02@hotmail.com>
2019-06-17 15:19:09 -0500

    Creando las primeras rutas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home Course';
+});
+
+Route::get('/usuarios', function () {
+    return 'Home usuarios';
+});
+Route::get('/usuarios/{id}', function ($id) {
+    return 'Mostrando detalles del usuario : '.$id;
+})->where('id','[0-9]+');
+Route::get('/usuarios/nuevo', function () {
+    return 'Creando al usuario : ';
+});
+Route::get('/saludo/{name}/{nickname?}', function ($name,$nickname=null) {
+    if($nickname){
+        return 'Tu nombre es : '.$name.' y tu apodo es '.$nickname;
+    }else{
+        return 'Tu nombre es '.$name;
+    }
+  
 });

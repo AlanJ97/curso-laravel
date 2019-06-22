606631eda9e3fce21fccd14eb373e39d955c26af
Alan <alannavaseg_02@hotmail.com>
2019-06-22 12:15:57 -0500

    Creación de las llaves foranes con el primer metodo en laravel

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id');            
+            $table->unsignedBigInteger('profession_id');         
+            $table->foreign('profession_id')->references('id')->on('professions');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+           
         });
     }
 

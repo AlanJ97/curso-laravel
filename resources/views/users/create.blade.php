098500e05a9be8c3af9c359d62f075b20e34bf94
Alan <alannavaseg_02@hotmail.com>
2019-06-28 16:47:58 -0500

    Styde.net

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,14 +1,32 @@
 @extends('layout')
 @section('title',"Crear usuario") 
 @section('content')
-<h2>Crear nuevo usuario</h2>           
+<h2>Crear nuevo usuario</h2>
+          
+@if ($errors -> any())
+    <p>¡Hay errores!</p>
+    <div class="alert alert-danger">
+        <h6>Por favor corrige lo siguiente:</h6>
+        {{-- <ul>
+            @foreach ($errors->all() as $error)
+                <li>
+                    {{$error}}
+                </li>     
+            @endforeach
+        </ul>     --}}
+    </div>
+    
+@endif
 <form action="{{url('usuarios/crear')}}" method="post">
     {!! csrf_field()!!}
     <label for="nombre">Nombre</label>
-    <input type="text" name="name" id="name">
+    <input type="text" name="name" id="name" value="{{old('name')}}">
+    @if ($errors -> has('name'))
+        <p>{{$errors -> first('name')}}</p>        
+    @endif
     <br>
     <label for="email">Email</label>
-    <input type="email" name="email" id="email">
+    <input type="email" name="email" id="email" value="{{old('email')}}">
     <br> 
     <label for="password">Contraseña</label>
     <input type="password" name="password" id="password">
